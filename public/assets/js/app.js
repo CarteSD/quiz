d37293df94b4a680cdf1a4694380cff8ab8fcf5c
Estéban DESESSARD <edesessard@iutbayonne.univ-pau.fr>
2025-01-09 17:30:42 +0100

    :memo: Documentation du fichier client `app.js`

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -10,6 +10,7 @@ const socket = io({
     }
 });
 
+// Récupération de tous les éléments du DOM qui sont manipulés
 const messageInput = document.getElementById('message-input');
 const messagesDiv = document.getElementById('messages');
 const sendBtn = document.getElementById('send-btn');
@@ -17,12 +18,11 @@ const imgPersonality = document.getElementById('img-personality');
 const question = document.getElementById('question');
 const leaderboard_players = document.getElementById('leaderboard-players');
 const timer = document.getElementById('timer');
+const timerCircle = document.querySelector('.timer-circle');
+const timerNumber = document.getElementById('timer');
 
+// Initialisation de la variable playerName
 let playerName = null;
-
-socket.on('connect', () => {
-    socket.emit('chat message', 'Un nouvel utilisateur s\'est connecté');
-});
 
 // Fonction permettant l'envoi du message au serveur via le socket
 function sendMessage() {
@@ -41,56 +41,80 @@ messageInput.addEventListener('keypress', (e) => {
     }
 });
 
-// Fonction traitant la réception du signal 'message' provenant du serveur
-socket.on('message', ({playerName, msg}) => {
-    if (playerName === 'System') {
-        let messageParagraph = document.createElement('p');
-        messageParagraph.classList.add("text-center");
-        if (msg.includes("Mauvaise réponse")) {
-            messageParagraph.classList.add("text-red-400");
-        }
-        if (msg.includes("Bonne réponse")) {
-            messageInput.disabled = true;
-            sendBtn.disabled = true;
-        }
-        messageParagraph.innerText = msg;
-        messagesDiv.appendChild(messageParagraph);
-    }
-    else {
-        let messageParagraph = document.createElement('p');
-        let span = document.createElement('span');
-        span.classList.add('font-bold');
-        span.innerText = playerName;
-        messageParagraph.appendChild(span);
-        let message = document.createTextNode(` : ${msg}`);
-        messageParagraph.appendChild(message);
-        messagesDiv.appendChild(messageParagraph);
-    }
-    messagesDiv.scrollTop = messagesDiv.scrollHeight; // Permet de mettre le scroll en bas
-});
-
-socket.on('new round', ({roundNumber, personality, timeLeft}) => {
-    imgPersonality.classList.remove('hidden');
-    question.classList.remove('hidden');
-    messageInput.disabled = false;
-    sendBtn.disabled = false;
-    messageInput.focus();
-    timer.innerHTML = timeLeft;
-    timer.classList.remove('text-red-500');
-    messagesDiv.innerHTML += `<p class="font-bold">Manche n°${roundNumber}</p>`;
-    messagesDiv.innerHTML += `<p>Indice : ${personality.hint}</p>`;
-    imgPersonality.src = `${personality.image}`;
-    messagesDiv.scrollTop = messagesDiv.scrollHeight; // Permet de mettre le scroll en bas
-})
-
+// Fonction traitant la réception du signal 'join' provenant du serveur
 socket.on('join', (pseudonyme) => {
     messagesDiv.innerHTML += `<p class="text-center">Vous avez rejoint la partie...</p>`;
     playerName = pseudonyme;
     messagesDiv.scrollTop = messagesDiv.scrollHeight; // Permet de mettre le scroll en bas
 });
 
+// Fonction traitant la réception du signal 'message' provenant du serveur
+socket.on('message', ({playerName, msg}) => {
+    let messageParagraph = document.createElement('p');
+    // Vérification si le message est envoyé par le système
+    if (playerName === 'System') {
+        messageParagraph.classList.add("text-center");
+
+        // Vérification du cas où le serveur annonce d'une mauvaise réponse proposée
+        if (msg.includes("Mauvaise réponse")) {
+            messageParagraph.classList.add("text-red-400");
+        }
+
+        // Vérification du cas où le serveur annonce d'une bonne réponse proposée
+        if (msg.includes("Bonne réponse")) {
+            messageInput.disabled = true;
+            sendBtn.disabled = true;
+        }
+
+        // Ajoute le contenu du message au paragraphe et ajoute le paragraphe à la div des messages
+        messageParagraph.innerText = msg;
+        messagesDiv.appendChild(messageParagraph);
+    }
+    else { // Si ce n'est pas le système (donc un joueur)
+        // Affichage du pseudonyme du joueur dans un span en gras
+        let span = document.createElement('span');
+        span.classList.add('font-bold');
+        span.innerText = playerName;
+        messageParagraph.appendChild(span);
+
+        // Affichage du message du joueur à la suite du pseudonyme
+        let message = document.createTextNode(` : ${msg}`);
+        messageParagraph.appendChild(message);
+
+        // Ajout du message entier à la div des messages
+        messagesDiv.appendChild(messageParagraph);
+    }
+    messagesDiv.scrollTop = messagesDiv.scrollHeight; // Permet de mettre le scroll en bas
+});
+
+// Fonction traitant la réception du signal 'new round' provenant du serveur
+socket.on('new round', ({roundNumber, personality, timeLeft}) => {
+    // Affiche l'image de la personnalité et le message de l'indice
+    imgPersonality.classList.remove('hidden');
+    question.classList.remove('hidden');
+
+    // Active la zone de saisi de réponse
+    messageInput.disabled = false;
+    sendBtn.disabled = false;
+    messageInput.focus();
+
+    // Initialise le timer et enlève la couleur rouge de la manche précédente
+    timer.innerHTML = timeLeft;
+    timer.classList.remove('text-red-500');
+
+    // Affiche le numéro de la manche et l'image de la personnalité avec l'indice
+    messagesDiv.innerHTML += `<p class="font-bold">Manche n°${roundNumber}</p>`;
+    messagesDiv.innerHTML += `<p>Indice : ${personality.hint}</p>`;
+    imgPersonality.src = `${personality.image}`;
+
+    messagesDiv.scrollTop = messagesDiv.scrollHeight; // Permet de mettre le scroll en bas
+})
+
+// Fonction traitant la réception du signal 'update leaderboard' provenant du serveur
 socket.on('update leaderboard', (leaderboard) => {
     leaderboard_players.innerHTML = ''; // Réinitialise le contenu
+
+    // Parcours le tableau des joueurs et les ajoute à la liste
     leaderboard.forEach((player, index) => {
         let rowP = document.createElement('p');
         let medal = '';
@@ -107,43 +131,51 @@ socket.on('update leaderboard', (leaderboard) => {
                 medal = '🥉・';
                 break;
         }
+
+        // Ajoute le contenu du message au paragraphe et ajoute le paragraphe à la div des messages
         rowP.innerText = `${medal}${player.username} : ${player.score} point(s)`;
         leaderboard_players.appendChild(rowP);
     });
 });
 
+// Fonction traitant la réception du signal 'timer' provenant du serveur
 socket.on('timer', ({totalTime, timeLeft}) => {
-    const timerCircle = document.querySelector('.timer-circle');
-    const timerNumber = document.getElementById('timer');
-
-    // On inverse le calcul du progress pour que ça se vide
+    // On calcule le pourcentage de cercle à afficher
     const progress = ((totalTime - timeLeft) / totalTime) * 360;
     timerCircle.style.setProperty('--progress', `${progress}deg`);
 
+    // Si le timer est égal à 0 (plus de temps restant), on désactive la zone de saisi et on vide le champ
     if (timeLeft === 0) {
         messageInput.disabled = true;
         messageInput.value = '';
         sendBtn.disabled = true;
     }
 
+    // Si le timer est inférieur ou égal à 5, on change la couleur du timer en rouge
     if (timeLeft > 0 && timeLeft <= 5) {
         timerNumber.classList.add('text-red-500');
         timerCircle.classList.add('text-red-500');
+
         // Le cercle en rouge qui se vide
         timerCircle.style.background = `conic-gradient(rgba(239, 68, 68, 0.2) var(--progress), transparent 0deg)`;
     }
 
+    // Sinon le timer est supérieur à 5, on enlève la couleur rouge
     if (timeLeft > 5) {
         timerNumber.classList.remove('text-red-500');
         timerCircle.classList.remove('text-red-500');
+
         // Le cercle normal qui se vide
         timerCircle.style.background = `conic-gradient(rgba(200, 200, 200, 0.2) var(--progress), transparent 0deg)`;
     }
 
+    // Affiche le temps restant dans le timer
     timerNumber.textContent = timeLeft;
 });
 
+// Fonction traitant la réception du signal 'end game' provenant du serveur
 socket.on('end game', () => {
+    // Cache l'image de la personnalité et le message de l'indice
     imgPersonality.classList.add('hidden');
     question.classList.add('hidden');
 });
